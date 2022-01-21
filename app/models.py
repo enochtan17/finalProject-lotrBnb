70475a7ae3837dbe3ad74dcb27894ea162f5d0b0
Enoch Tan <enochtan@umich.edu>
2022-01-21 03:20:29 -0500

    rivendell listing seed added

--- app/models.py
+++ app/models.py
@@ -59,7 +59,7 @@ class Listing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    description =  db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
     address = db.Column(db.String(255), nullable=False)
     city = db.Column(db.String(100), nullable=False)
     country = db.Column(db.String(100), nullable=False)
--- app/seeds/listings.py
+++ app/seeds/listings.py
@@ -2,19 +2,55 @@ from app.models import db, Listing
 
 def seed_listings():
     demo_listing1 = Listing(
-
+        owner_id = 7
+        name = 'Imladris'
+        description = 'Secluded sanctuary in a beautiful hidden valley. Come rest under the protection of Elven magic and enjoy a peace of mind you cannot experience anywhere else in Middle Earth. Hike along the deep river valley and gaze upon breathtaking waterfalls. Three meals a day and live serene music included.'
+        address = '1697 Peredhil Way'
+        city = 'Rivendell'
+        country = 'Eriador'
+        latitude = -41.06
+        longitude = 175.19
+        price = 120
+        image_url = 'https://www.wallpapertip.com/wmimgs/54-540978_lord-of-the-rings-rivendell.png'
     )
 
     demo_listing2 = Listing(
-
+        owner_id = ''
+        name = ''
+        description = ''
+        address = ''
+        city = ''
+        country = ''
+        latitude = ''
+        longitude = ''
+        price = ''
+        image_url = ''
     )
 
     demo_listing3 = Listing(
-
+        owner_id = ''
+        name = ''
+        description = ''
+        address = ''
+        city = ''
+        country = ''
+        latitude = ''
+        longitude = ''
+        price = ''
+        image_url = ''
     )
 
     demo_listing4 = Listing(
-
+        owner_id = ''
+        name = ''
+        description = ''
+        address = ''
+        city = ''
+        country = ''
+        latitude = ''
+        longitude = ''
+        price = ''
+        image_url = ''
     )
 
     db.session.add(demo_listing1)
