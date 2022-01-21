01bf3a1a1ff662c9cd333666e50110c011594bac
Enoch Tan <enochtan@umich.edu>
2022-01-20 23:30:08 -0500

    listing and booking routes done

--- app/api/bookings.py
+++ app/api/bookings.py
@@ -6,3 +6,63 @@ from app.forms.booking_form import BookingForm
 from app.forms.review_form import ReviewForm
 
 from .auth_routes import validation_errors_to_error_messages
+
+booking_routes = Blueprint('bookings', __name__)
+
+
+# GET all bookings for one listing
+
+@booking_routes.route('/listing/<int:listingId>')
+@login_required
+def get_bookings(listingId):
+    bookings = Booking.query.filter(Booking.listing_id == listingId).all()
+    return {'bookings': [booking.to_dict() for booking in bookings]}
+
+
+# POST a booking
+
+@booking_routes.route('/listing/<int:listingId>', methods=['POST'])
+@login_required
+def add_booking(listingId):
+    form = BookingForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        booking = Booking(
+            listing_id = listingId
+            guest_id = current_user.id
+            start_date = form.start_date.data
+            end_date = form.end_date.data
+            num_guests = form.num_guests.data
+        )
+        db.session.add(booking)
+        db.session.commit()
+        return booking.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+# EDIT a booking
+
+@booking_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def edit_booking(id):
+    form = BookingForm()
+    booking = Booking.query.get(id)
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        booking.start_date = form.start_date.data
+        booking.end_date = form.end_date.data
+        booking.num_guests = form.num_guests.data
+        db.session.commit()
+        return booking.to_dict()
+    return {}
+
+
+# DELETE a booking
+
+@booking_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_booking(id):
+    booking = Booking.query.filter_by(id=id).first()
+    db.session.delete(booking)
+    db.session.commit()
+    return booking.to_dict()
--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, DateField
 from wtforms.validators import DataRequired
 
-class ReviewForm(FlaskForm):
+class BookingForm(FlaskForm):
     start_date = DateField('start_date', validators=[DataRequired()])
     end_date = DateField('end_date', validators=[DataRequired()])
     num_guests = IntegerField('num_guests', validators=[DataRequired()])
