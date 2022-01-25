4d6e6ccfb138871006c699daf37fa3951c14558d
Enoch Tan <enochtan@umich.edu>
2022-01-25 18:32:46 -0500

    logout properly navigates to splash element

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-// import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { logout } from '../../store/session'
 
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
 
   const onLogout = async (e) => {
     await dispatch(logout())
-    // navigate('/splash')
+    navigate('/')
   }
 
   return <button onClick={onLogout}>Logout</button>
