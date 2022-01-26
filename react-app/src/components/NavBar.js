825b983d528cbbe9e05d7e1e4bde3cf3278763d2
Enoch Tan <enochtan@umich.edu>
2022-01-26 00:41:15 -0500

    navbar styling started

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -8,31 +8,29 @@ const NavBar = () => {
 
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        { !userState && (
-          <>
-            <li>
-              <NavLink to='/login' exact={true} activeClassName='active'>
-                Login
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/sign-up' exact={true} activeClassName='active'>
-                Sign Up
-              </NavLink>
-            </li>
-          </>
-        )}
-        { userState && (
-        <li>
-          <LogoutButton />
-        </li> )}
-      </ul>
+      <div>
+        <NavLink to='/' exact={true} activeClassName='active'>
+          Home
+        </NavLink>
+      </div>
+      { !userState && (
+        <div className='auth-div'>
+          <div className='auth-button'>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              <img src="https://img.icons8.com/external-bearicons-flat-bearicons/25/000000/external-login-call-to-action-bearicons-flat-bearicons.png"/>
+            </NavLink>
+          </div>
+          <div className='auth-button'>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              <img src="https://img.icons8.com/external-bearicons-outline-color-bearicons/25/000000/external-sign-up-call-to-action-bearicons-outline-color-bearicons-1.png"/>
+            </NavLink>
+          </div>
+        </div>
+      )}
+      { userState && (
+      <div className='logout-div'>
+        <LogoutButton />
+      </div> )}
     </nav>
   )
 }
