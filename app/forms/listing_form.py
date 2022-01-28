77d942e029562742d5adfef324350e6be517fca3
Enoch Tan <enochtan@umich.edu>
2022-01-27 19:54:25 -0500

    show errors in new listing

--- app/forms/listing_form.py
+++ app/forms/listing_form.py
@@ -2,19 +2,38 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, TextAreaField, DecimalField
 from wtforms.validators import DataRequired
 
+def is_int(form, field):
+    # Checking if field is integer
+    value = field.data
+    if type(value) != int:
+        raise ValidationError(str(field) + ' is not a valid integer.')
+
+def is_float(form, field):
+    # Checking if field is float
+    value = field.data
+    if type(value) != float:
+        raise ValidationError(str(field) + ' is not a valid number.')
+
 class ListingForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     description = TextAreaField('description', validators=[DataRequired()])
-    capacity = IntegerField('capacity', validators=[DataRequired()])
-    bedrooms = IntegerField('bedrooms', validators=[DataRequired()])
-    beds = IntegerField('beds', validators=[DataRequired()])
-    baths = IntegerField('baths', validators=[DataRequired()])
+    capacity = IntegerField('capacity', validators=[DataRequired(),
+                                                    is_int])
+    bedrooms = IntegerField('bedrooms', validators=[DataRequired(),
+                                                    is_int])
+    beds = IntegerField('beds', validators=[DataRequired(),
+                                                    is_int])
+    baths = IntegerField('baths', validators=[DataRequired(),
+                                                    is_int])
     address = StringField('address', validators=[DataRequired()])
     city = StringField('city', validators=[DataRequired()])
     country = StringField('country', validators=[DataRequired()])
-    latitude = DecimalField('latitude', validators=[DataRequired()])
-    longitude = DecimalField('longitude', validators=[DataRequired()])
-    price = IntegerField('price', validators=[DataRequired()])
+    latitude = DecimalField('latitude', validators=[DataRequired(),
+                                                    is_float])
+    longitude = DecimalField('longitude', validators=[DataRequired(),
+                                                    is_float])
+    price = IntegerField('price', validators=[DataRequired(),
+                                                    is_int])
     image_url = StringField('image_url', validators=[DataRequired()])
 
 
--- react-app/src/components/NewListingForm/index.js
+++ react-app/src/components/NewListingForm/index.js
@@ -22,10 +22,9 @@ function NewListingForm() {
     const [longitude, setLongitude] = useState('')
     const [price, setPrice] = useState('')
     const [imageUrl, setImageUrl] = useState('')
-    const [errors, setErrors] = useState([])
 
     const createListing = async e => {
-        const data = await dispatch(addNewListing(
+        await dispatch(addNewListing(
             listingName,
             description,
             capacity,
@@ -40,10 +39,60 @@ function NewListingForm() {
             price,
             imageUrl
         ))
-        if (data) {
-            console.log('data', data)
-            setErrors(data)
-        }
+    }
+
+    const [nameError, setNameError] = useState('')
+    const [descriptionError, setDescriptionError] = useState('')
+    const [capacityError, setCapacityError] = useState('')
+    const [bedroomError, setBedroomError] = useState('')
+    const [bedError, setBedError] = useState('')
+    const [bathError, setBathError] = useState('')
+    const [addressError, setAddressError] = useState('')
+    const [cityError, setCityError] = useState('')
+    const [countryError, setCountryError] = useState('')
+    const [latitudeError, setLatitudeError] = useState('')
+    const [longitudeError, setLongitudeError] = useState('')
+    const [priceError, setPriceError] = useState('')
+    const [imageUrlError, setImageUrlError] = useState('')
+
+    const initNameError = () => {
+        setNameError('Name required')
+    }
+    const initDescriptionError = () => {
+        setDescriptionError('Description required')
+    }
+    const initCapacityError = () => {
+        setCapacityError('Capacity must be valid number')
+    }
+    const initBedroomError = () => {
+        setBedroomError('Bedrooms must be valid number')
+    }
+    const initBedError = () => {
+        setBedError('Beds must be valid number')
+    }
+    const initBathError = () => {
+        setBathError('Baths must be valid number')
+    }
+    const initAddressError = () => {
+        setAddressError('Address required')
+    }
+    const initCityError = () => {
+        setCityError('City required')
+    }
+    const initCountryError = () => {
+        setCountryError('Country required')
+    }
+    const initLatitudeError = () => {
+        setLatitudeError('Latitude must be valid number')
+    }
+    const initLongitudeError = () => {
+        setLongitudeError('Longitude must be valid number')
+    }
+    const initPriceError = () => {
+        setPriceError('Price must be valid number')
+    }
+    const initImageUrlError = () => {
+        setImageUrlError('Image URL required')
     }
 
     return (
@@ -51,30 +100,26 @@ function NewListingForm() {
             { showForm && (
                 <form
                     className={classes}
-                    onSubmit={async(e) => {
+                    onSubmit={
+                        async(e) => {
                         e.preventDefault()
-                        if (listingName && description &&
-                            capacity && bedrooms && beds && baths &&
-                            address && city && country &&
-                            latitude && longitude &&
-                            price && imageUrl) {
-                            dispatch(addListingOff())
-                            await createListing()
-                            setListingName('')
-                            setDescription('')
-                            setCapacity('')
-                            setBedrooms('')
-                            setBeds('')
-                            setBaths('')
-                            setAddress('')
-                            setCity('')
-                            setCountry('')
-                            setLatitude(0)
-                            setLongitude(0)
-                            setPrice(0)
-                            setImageUrl('')
-                        }
-                    }}
+                        await createListing()
+                        dispatch(addListingOff())
+                        setListingName('')
+                        setDescription('')
+                        setCapacity('')
+                        setBedrooms('')
+                        setBeds('')
+                        setBaths('')
+                        setAddress('')
+                        setCity('')
+                        setCountry('')
+                        setLatitude('')
+                        setLongitude('')
+                        setPrice('')
+                        setImageUrl('')
+                    }
+                }
                 >
                     <div className='form1'>
                         <h2>Create New Listing</h2>
@@ -82,119 +127,145 @@ function NewListingForm() {
                         <input
                             placeholder='Listing Name'
                             value={listingName}
+                            onClick={initNameError}
                             onChange={e => {
                                 setListingName(e.target.value)
                             }}
                             required
                         ></input>
+                        { nameError ? <div className='err-modal'>{nameError}</div> : '' }
                         <label>Description</label>
                         <input
                             placeholder='Description'
                             value={description}
+                            onClick={initDescriptionError}
                             onChange={e => {
                                 setDescription(e.target.value)
                             }}
                             required
                         ></input>
+                        { descriptionError ? <div className='err-modal'>{descriptionError}</div> : '' }
                         <label>Capacity</label>
                         <input
                             placeholder='Capacity'
                             value={capacity}
+                            onClick={initCapacityError}
                             onChange={e => {
                                 setCapacity(e.target.value)
                             }}
                             required
                         ></input>
+                        { capacityError ? <div className='err-modal'>{capacityError}</div> : '' }
                         <label>Bedrooms</label>
                         <input
                             placeholder='Bedrooms'
                             value={bedrooms}
+                            onClick={initBedroomError}
                             onChange={e => {
                                 setBedrooms(e.target.value)
                             }}
                             required
                         ></input>
+                        { bedroomError ? <div className='err-modal'>{bedroomError}</div> : '' }
                         <label>Beds</label>
                         <input
                             placeholder='Beds'
                             value={beds}
+                            onClick={initBedError}
                             onChange={e => {
                                 setBeds(e.target.value)
                             }}
                             required
                         ></input>
+                        { bedError ? <div className='err-modal'>{bedError}</div> : '' }
                         <label>Baths</label>
                         <input
                             placeholder='Baths'
                             value={baths}
+                            onClick={initBathError}
                             onChange={e => {
                                 setBaths(e.target.value)
                             }}
                             required
                         ></input>
+                        { bathError ? <div className='err-modal'>{bathError}</div> : '' }
                         <label>Address</label>
                         <input
                             placeholder='Address'
                             value={address}
+                            onClick={initAddressError}
                             onChange={e => {
                                 setAddress(e.target.value)
                             }}
                             required
                         ></input>
+                        { addressError ? <div className='err-modal'>{addressError}</div> : '' }
                         <label>City</label>
                         <input
                             placeholder='City'
                             value={city}
+                            onClick={initCityError}
                             onChange={e => {
                                 setCity(e.target.value)
                             }}
                             required
                         ></input>
+                        { cityError ? <div className='err-modal'>{cityError}</div> : '' }
                         <label>Country</label>
                         <input
                             placeholder='Country'
                             value={country}
+                            onClick={initCountryError}
                             onChange={e => {
                                 setCountry(e.target.value)
                             }}
                             required
                         ></input>
+                        { countryError ? <div className='err-modal'>{countryError}</div> : '' }
                         <label>Latitude</label>
                         <input
                             placeholder='Latitude'
                             value={latitude}
+                            onClick={initLatitudeError}
                             onChange={e => {
                                 setLatitude(e.target.value)
                             }}
                             required
                         ></input>
+                        { latitudeError ? <div className='err-modal'>{latitudeError}</div> : '' }
                         <label>Longitude</label>
                         <input
                             placeholder='Longitude'
                             value={longitude}
+                            onClick={initLongitudeError}
                             onChange={e => {
                                 setLongitude(e.target.value)
                             }}
                             required
                         ></input>
+                        { longitudeError ? <div className='err-modal'>{longitudeError}</div> : '' }
                         <label>Price</label>
                         <input
                             placeholder='Price'
                             value={price}
+                            onClick={initPriceError}
                             onChange={e => {
                                 setPrice(e.target.value)
                             }}
                             required
                         ></input>
+                        { priceError ? <div className='err-modal'>{priceError}</div> : '' }
                         <label>Image URL</label>
                         <input
                             placeholder='Image URL'
                             value={imageUrl}
+                            onClick={initImageUrlError}
                             onChange={e => {
                                 setImageUrl(e.target.value)
                             }}
                             required
                         ></input>
+                        { imageUrlError ? <div className='err-modal'>{imageUrlError}</div> : '' }
                     </div>
                     <div className='listingButton'>
                         <p
