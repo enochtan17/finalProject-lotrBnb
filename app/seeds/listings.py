134c50fcacb43233f60c208033805445071e0d90
Enoch Tan <enochtan@umich.edu>
2022-01-30 14:02:48 -0500

    some seeds added

--- app/seeds/listings.py
+++ app/seeds/listings.py
@@ -18,29 +18,37 @@ def seed_listings():
         image_url = 'https://wallpapercave.com/wp/wp1947157.jpg'
     )
 
-    # demo_listing2 = Listing(
-    #     owner_id = ''
-    #     name = ''
-    #     description = ''
-    #     address = ''
-    #     city = ''
-    #     country = ''
-    #     latitude = ''
-    #     longitude = ''
-    #     price = ''
-    #     image_url = ''
-    # )
+    bag_end = Listing(
+        owner_id = 10,
+        name = 'Bag End',
+        description = 'The finest hobbit-hole in the South Farthing. Fully stocked with food and drink. Furnished with treasures recovered from Smaug at the Lonely Mountain. Beautiful gardens kept up by Samwise Gamgee. Just a stone\'s throw away from the Green Dragon.',
+        capacity = 15,
+        bedrooms = 8,
+        beds = 12,
+        baths = 6,
+        address = '404 Bagshot Row',
+        city = 'Hobbiton',
+        country = 'Shire',
+        latitude = -37.86,
+        longitude = 175.68,
+        price = 400,
+        image_url = 'https://i.insider.com/5575ad2d6da811950fb7ee96?width=1000&format=jpeg&auto=webp'
+    )
 
     # demo_listing3 = Listing(
-    #     owner_id = ''
-    #     name = ''
-    #     description = ''
-    #     address = ''
-    #     city = ''
-    #     country = ''
-    #     latitude = ''
-    #     longitude = ''
-    #     price = ''
+    #     owner_id = '',
+    #     name = '',
+    #     description = '',
+    #     address = '',
+    #     capacity = '',
+    #     bedrooms = '',
+    #     beds = '',
+    #     baths = '',
+    #     city = '',
+    #     country = '',
+    #     latitude = '',
+    #     longitude = '',
+    #     price = '',
     #     image_url = ''
     # )
 
@@ -58,7 +66,7 @@ def seed_listings():
     # )
 
     db.session.add(rivendell)
-    # db.session.add(demo_listing2)
+    db.session.add(bag_end)
     # db.session.add(demo_listing3)
     # db.session.add(demo_listing4)
 
--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -12,11 +12,51 @@ def seed_reviews():
         listing_id = 1,
         guest_id = 10,
         rating = 5,
-        comment = 'Came here to retire and work on my book - was not disappointed and could not think of a better retirement.'
+        comment = 'Came here to retire and work on my book - was not disappointed and could not think of a better way to spend my time.'
+    )
+
+    review3 = Review(
+        listing_id = 1,
+        guest_id = 3,
+        rating = 5,
+        comment = 'Great communcation and hospitality. Always good to see an old friend.'
+    )
+
+    review4 = Review(
+        listing_id = 1,
+        guest_id = 6,
+        rating = 3,
+        comment = 'People rave about the serene beauty of Rivendell but it is nothing like the mountain halls of our great mines. Plus the food was a little lacking - all green stuff.'
+    )
+
+    review5 = Review(
+        listing_id = 1,
+        guest_id = 11,
+        rating = 5,
+        comment = 'Elrond\'s home is an inspiration for other Elf-hosts like myself. Certainly lives up to its name of The Last Homely House East of the Sea.'
+    )
+
+    review6 = Review(
+        listing_id = 2,
+        guest_id = 3,
+        rating = 4,
+        comment = 'One of the coziest and cheery places I have ever stayed at, although I did knock my head on the low ceilings a few times, which will result in the knocking off of one star.'
+    )
+
+    review7 = Review(
+        listing_id = 2,
+        guest_id = 2,
+        rating = 5,
+        comment = 'My uncle is a great host. What people may see as a cluttered mess is really just organized chaos. Ever since that trip to the mountains, he has been very meticulous about keeping things in order... particularly a peculiar golden ring of his.'
     )
 
     db.session.add(review1)
     db.session.add(review2)
+    db.session.add(review3)
+    db.session.add(review4)
+    db.session.add(review5)
+    db.session.add(review6)
+    db.session.add(review7)
     db.session.commit()
 
 def undo_reviews():
