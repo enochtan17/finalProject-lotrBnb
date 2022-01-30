613bfaf3da15f0c76ba85fb61f04bb807c165732
Enoch Tan <enochtan@umich.edu>
2022-01-30 01:49:18 -0500

    404 page done

--- react-app/src/components/NotFound/index.js
+++ react-app/src/components/NotFound/index.js
@@ -4,10 +4,8 @@ import './notFound.css'
 function NotFound() {
     return (
         <div className='not-found-container'>
-            <h1>404 not found page</h1>
-            <h1>404 not found page</h1>
-            <h1>404 not found page</h1>
-            <a href="/">Go Back</a>
+            <h1>You look more lost than Frodo and Sam in <br/> Emyn Muil. Don't go in circles - go 
+            <a href="/"> home.</a></h1>
         </div>
     )
 }
