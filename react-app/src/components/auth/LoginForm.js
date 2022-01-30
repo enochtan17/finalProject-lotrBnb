5538e0ff3f74d347433e4d9fdefc35c6a45d0cb4
Enoch Tan <enochtan@umich.edu>
2022-01-30 03:42:22 -0500

    login fail conditionals added;

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -7,13 +7,20 @@ import sauronIcon from '../../zzimages/sauron_icon/favicon.ico'
 import eyeSlash from '../../zzimages/eye-slash-icon/favicon.ico'
 
 const LoginForm = () => {
+  const user = useSelector(state => state.session.user)
+  const dispatch = useDispatch()
+
   const [errors, setErrors] = useState([])
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [typePass, setTypePass] = useState('password')
   const [sauron, setSauron] = useState(true)
-  const user = useSelector(state => state.session.user)
-  const dispatch = useDispatch()
+
+  const [classLoginFail, setClassLoginFail] = useState('')
+  const [classWB, setClassWB] = useState('auth-header')
+  const [errorBox, setErrorBox] = useState('')
+  const [errorDiv, setErrorDiv] = useState('error')
+  const [gandalf, setGandalf] = useState('none')
 
   const changeField = () => {
     if (typePass === 'password') {
@@ -31,6 +38,17 @@ const LoginForm = () => {
     const data = await dispatch(login(email, password))
     if (data) {
       setErrors(data)
+      setClassLoginFail('login-fail')
+      setClassWB('auth-header fail-wb')
+      setErrorBox('error-box')
+      setErrorDiv('error-fail')
+      setGandalf('gandalf')
+    } else {
+      setClassLoginFail('')
+      setClassWB('auth-header')
+      setErrorBox('')
+      setErrorDiv('error')
+      setGandalf('none')
     }
   }
 
@@ -54,48 +72,51 @@ const LoginForm = () => {
   return (
     <>
       <NavBar />
-      <h1 className='auth-header'>Welcome Back!</h1>
-      <form onSubmit={onLogin} className='auth-form'>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind} className='error'>{error}</div>
-          ))}
-        </div>
-        <Link to="/sign-up" className='carousel'>Don't have an account?</Link>
-        <div className='label-input'>
-          <input
-            name='email'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={updateEmail}
-          />
-        </div>
-        <div className='label-input'>
-          <input
-            name='password'
-            type={typePass}
-            placeholder='Password'
-            value={password}
-            onChange={updatePassword}
-          />
-          { sauron ? <img
-            src={sauronIcon}
-            id='toggle-password'
-            onClick={changeField}
-            alt=''></img>
-            : <img
-            src={eyeSlash}
-            id='toggle-password'
-            onClick={changeField}
-            alt=''></img>
-          }
-        </div>
-        <div className='login-button-container'>
-          <button className='login-demo-button' type='submit'>Login</button>
-          <button className='login-demo-button' onClick={demoLogin}>Demo</button>
-        </div>
-      </form>
+      <div className={classLoginFail}>
+        <h1 className={classWB}>Welcome Back!</h1>
+        <form onSubmit={onLogin} className='auth-form'>
+          <div className={errorBox}>
+            {errors.map((error, ind) => (
+              <div key={ind} id={ind} className={errorDiv}>{error}</div>
+            ))}
+          </div>
+          <Link to="/sign-up" className='carousel'>Don't have an account?</Link>
+          <div className='label-input'>
+            <input
+              name='email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div className='label-input'>
+            <input
+              name='password'
+              type={typePass}
+              placeholder='Password'
+              value={password}
+              onChange={updatePassword}
+            />
+            { sauron ? <img
+              src={sauronIcon}
+              id='toggle-password'
+              onClick={changeField}
+              alt=''></img>
+              : <img
+              src={eyeSlash}
+              id='toggle-password'
+              onClick={changeField}
+              alt=''></img>
+            }
+          </div>
+          <div className='login-button-container'>
+            <button className='login-demo-button' type='submit'>Login</button>
+            <button className='login-demo-button' onClick={demoLogin}>Demo</button>
+          </div>
+        </form>
+        <h1 className={gandalf}>You shall not pass!</h1>
+      </div>
     </>
   )
 }
